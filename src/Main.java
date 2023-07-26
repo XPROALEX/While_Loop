90db9fb2703d5daa7d7597f0c76bd98c0fd4f003
Alex <alex97av@gmail.com>
2023-07-26 11:53:23 +0200

    aggiunta while and if else con print

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main (String[] args){
         int startBotPounds=10000;
         int totalDayToSellAll=0;
+        while(startBotPounds>0){
+            double exchangeRate=  (1 + (Math.random() * 0.2));
+            if (exchangeRate>=1.15){
+                startBotPounds-=1000;
+                totalDayToSellAll++;}
+            else if (exchangeRate<1.15) {
+                totalDayToSellAll++;
+            }
+        }
+        System.out.println("Days to sell all pounds: "+totalDayToSellAll);
     }
 }
